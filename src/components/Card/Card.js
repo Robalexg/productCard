a5eedf3cbadcca8d8a98d7d72b7e671ab12629f7
robalexg <alexg7070@yahoo.com>
2022-10-20 12:55:46 -0500

    bulding components and styling

---
+++ src/components/Card/Card.js
@@ -0,0 +1,24 @@
+import './Card.css'
+
+const Card = () => {
+    return (
+        <section className="card--container">
+            <div className='card--imgContainer' >
+                <img className='card--img' src="../images/image-product-desktop.jpg" alt="" />
+            </div>
+            <div className='card--textContainer'>
+                <h2 className='card--type' >PERFUME</h2>
+                <h1 className='card--title' >Gabrielle Essence Eau De Parfum</h1>
+                <h3 className='card--desc' >
+                    A floral, solar and voluptuous interpretation composed by
+                    Olivier Polge, Perfumer-Creator for the house of CHANEL.
+
+                </h3>
+                <p className='card--price' >$149.99 <span className='grey' >$169.99</span></p>
+                <button className='card--button' ><img src="../images/icon-cart.svg" alt="" />Add to Cart</button>
+            </div>
+        </section>
+    )
+}
+
+export default Card
